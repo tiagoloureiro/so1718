f789512db25e19547da87b2ba8db011cd3bd789d
João Silva <johnnysilva93@gmail.com>
2018-05-29 02:09:44 +0100

    Added function that creats a random string of 8 caracters

--- lib/helpers.h
+++ lib/helpers.h
@@ -13,10 +13,12 @@ ssize_t readln(int fildes, void *buffer, size_t nbyte);
 
 size_t gatherArg(char* arg[], char* buffer, size_t size);
 
+void randomName(char* dir);
+
 int analyse(char* buffer, ssize_t size);
 
-void execute(char* arg[],  ssize_t num);
+void execute(char* arg[],  ssize_t num, int execs);
 
-void executePipe(char* arg[], ssize_t num);
+void executePipe(char* arg[], ssize_t num, int execs);
 
 void printline(char* buffer, size_t n);
--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "lib/helpers.h"
 
 int main(int argc, char *argv[]) {
-	
+
 	// Open file that is received as an argument by the program
 	int fd = open(argv[1], O_RDWR, 0640);	
 	// Creates pipeline to save last command result
 	mkfifo("Pipeline", 0640);
 	open("execBefore.txt", O_CREAT | O_TRUNC, 0640);
 	open("tmp.txt", O_CREAT | O_TRUNC, 0640);
+	char dir[8];
+	randomName(dir);
 	int fd2 = open("error.txt", O_CREAT | O_WRONLY | O_TRUNC, 0640);
 	if(fd < 0){
 		perror("The file does not exists\n");
@@ -25,6 +27,7 @@ int main(int argc, char *argv[]) {
 	}
 	size_t n;
 	int flag = 0;
+	int execs = 1;
 	while((n = readln(0, buffer, 1)) >= 1){
 		char *arg[32];
 		size_t num;
@@ -44,12 +47,14 @@ int main(int argc, char *argv[]) {
 				}
 				case 1:{
 					num = gatherArg(arg, buffer, n);
-					execute(arg, num);
+					execute(arg, num, execs);
+					execs++;
 					break;
 				}
 				case 2:{
 					num = gatherArg(arg, buffer, n);
-					executePipe(arg, num);
+					executePipe(arg, num, execs);
+					execs++;
 					break;
 				}
 			}
